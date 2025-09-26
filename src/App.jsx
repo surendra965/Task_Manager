fcb9ad999c551d6c8a5225740092bf860331afe2
Surendra <nelloresurendra0@gmail.com>
2025-09-26 23:01:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React , { useState } from 'react'
+import TaskInput from "./components/TaskInput";
+import TaskItem from "./components/TaskItem";
+
+function App() {
+  const[toDoList,setToDoList]=useState([]);
+
+  const addTask = (taskName) =>{
+    const newTask = { taskName, checked:false};
+    setToDoList([...toDoList, newTask]);
+  };
+  function deletTask(deletTaskName){
+    setToDoList(toDoList.filter(task=>task.
+      taskName!==deletTaskName))
+  }
+  function toggleCheck(taskName){
+    setToDoList((prevToDoList)=> 
+      prevToDoList.map((task)=>
+        task.taskName===taskName? {...task,
+          checked:!task.checked}:task,
+        ),
+      );
+  }
+  return (
+    <> 
+      <div className="container">
+        <h1>Task viewer</h1>
+        <TaskInput addTask={addTask} />
+        <div className="toDoList">
+          <span>To do</span>
+          <ul className="list-items">
+            {toDoList.map((task,key)=>(
+              <TaskItem  task={task} key={key} deletTask={deletTask} toggleCheck={toggleCheck} />
+            ))}
+          </ul>
+
+          {toDoList.length === 0 ? (
+            <p className="notify">You are done!</p>
+          ) : null}
+        </div>
+      </div>
+  </>
+  );
+}
+
+export default App;
